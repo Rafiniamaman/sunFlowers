f9c2c7c7ba66b73efea3f0a5bc21950974ad19c9
Rafał <rafalcwiklinski.1999@wp.pl>
2023-04-21 15:29:55 +0200

    added footer

--- js/main.js
+++ js/main.js
@@ -1,21 +1,28 @@
-const burgerBtn = document.querySelector('.burger-btn')
-const nav = document.querySelector('.nav')
-const navItem1 = document.querySelector('.nav__item-1')
-const navItem2 = document.querySelector('.nav__item-2')
-const navItem3 = document.querySelector('.nav__item-3')
-const navItem4 = document.querySelector('.nav__item-4')
+const burgerBtn = document.querySelector(".burger-btn");
+const nav = document.querySelector(".nav");
+const navItem1 = document.querySelector(".nav__item-1");
+const navItem2 = document.querySelector(".nav__item-2");
+const navItem3 = document.querySelector(".nav__item-3");
+const navItem4 = document.querySelector(".nav__item-4");
+const footer = document.querySelector(".footer");
+
+const footerYear = () => {
+	const today = new Date();
+	footer.innerHTML = `Copyright &copy; ${today.getFullYear()}`;
+};
+
+footerYear()
 
 
 const showNav = () => {
-    nav.classList.toggle('nav--active')
-}
+	nav.classList.toggle("nav--active");
+};
 const closeNav = () => {
-    nav.classList.toggle('nav--active')
-}
+	nav.classList.toggle("nav--active");
+};
 
-
-navItem1.addEventListener('click', closeNav)
-navItem2.addEventListener('click', closeNav)
-navItem3.addEventListener('click', closeNav)
-navItem4.addEventListener('click', closeNav)
-burgerBtn.addEventListener('click', showNav)
+navItem1.addEventListener("click", closeNav);
+navItem2.addEventListener("click", closeNav);
+navItem3.addEventListener("click", closeNav);
+navItem4.addEventListener("click", closeNav);
+burgerBtn.addEventListener("click", showNav);
